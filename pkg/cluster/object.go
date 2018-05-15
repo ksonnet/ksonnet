b5b8be135199cd177554541f7e8275d445535bdb
bryanl <bryanliles@gmail.com>
2018-05-15 16:23:12 -0400

    Fetch all ksonnet managed objects from cluster
    Signed-off-by: bryanl <bryanliles@gmail.com>

--- pkg/cluster/object.go
+++ pkg/cluster/object.go
@@ -15,7 +15,15 @@
 
 package cluster
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	"github.com/pkg/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/clientcmd"
+	kcmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
+	"k8s.io/kubernetes/pkg/kubectl/resource"
+)
 
 // SetMetaDataLabel sets a label value
 func SetMetaDataLabel(obj metav1.Object, key, value string) {
@@ -35,4 +43,59 @@ func SetMetaDataAnnotation(obj metav1.Object, key, value string) {
 	}
 	a[key] = value
 	obj.SetAnnotations(a)
+}
+
+// DefaultResourceInfo fetches objects from the cluster.
+func DefaultResourceInfo(namespace string, config clientcmd.ClientConfig) *resource.Result {
+	factory := kcmdutil.NewFactory(config)
+
+	return factory.NewBuilder().
+		Unstructured().
+		NamespaceParam(namespace).
+		ExportParam(false).
+		ResourceTypeOrNameArgs(true, "all").
+		LabelSelectorParam("app.kubernetes.io/deploy-manager=ksonnet").
+		ContinueOnError().
+		Flatten().
+		IncludeUninitialized(false).
+		RequireObject(true).
+		Latest().
+		Do()
+}
+
+type ResourceInfo interface {
+	Err() error
+	Infos() ([]*resource.Info, error)
+}
+
+var _ ResourceInfo = (*resource.Result)(nil)
+
+// ManagedObjects returns a slice of ksonnet managed objects.
+func ManagedObjects(r ResourceInfo) ([]*unstructured.Unstructured, error) {
+	if err := r.Err(); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	infos, err := r.Infos()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	lookup := make(map[types.UID]map[string]interface{})
+
+	var objects []*unstructured.Unstructured
+
+	for i := range infos {
+		if obj, ok := infos[i].Object.(*unstructured.Unstructured); ok {
+			if _, ok := lookup[obj.GetUID()]; ok {
+				// we've seen this object already
+				continue
+			}
+
+			lookup[obj.GetUID()] = obj.Object
+			objects = append(objects, obj)
+		}
+	}
+
+	return objects, nil
 }
--- pkg/cluster/object_test.go
+++ pkg/cluster/object_test.go
@@ -18,8 +18,11 @@ package cluster
 import (
 	"testing"
 
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/kubernetes/pkg/kubectl/resource"
 )
 
 func TestSetMetadataLabel(t *testing.T) {
@@ -136,4 +139,83 @@ func TestSetMetadataAnnotation(t *testing.T) {
 			require.Equal(t, tc.expected, obj.Object)
 		})
 	}
+}
+
+func TestManagedObjects(t *testing.T) {
+	obj1 := &unstructured.Unstructured{}
+	obj1.SetUID(types.UID("uid"))
+
+	obj2 := &unstructured.Unstructured{}
+	obj2.SetUID(types.UID("uid"))
+
+	obj3 := &unstructured.Unstructured{}
+	obj3.SetUID(types.UID("uid2"))
+
+	cases := []struct {
+		name         string
+		resourceInfo ResourceInfo
+		expected     []*unstructured.Unstructured
+		isErr        bool
+	}{
+		{
+			name: "no error",
+			resourceInfo: func() *fakeResourceInfo {
+				return &fakeResourceInfo{
+					infos: []*resource.Info{
+						{Object: obj1},
+						{Object: obj2},
+						{Object: obj3},
+					},
+				}
+			}(),
+			expected: []*unstructured.Unstructured{obj1, obj3},
+		},
+
+		{
+			name: "resource info error",
+			resourceInfo: func() *fakeResourceInfo {
+				return &fakeResourceInfo{
+					err: errors.New("error"),
+				}
+			}(),
+			isErr: true,
+		},
+
+		{
+			name: "infos error",
+			resourceInfo: func() *fakeResourceInfo {
+				return &fakeResourceInfo{
+					infosErr: errors.New("error"),
+				}
+			}(),
+			isErr: true,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			objects, err := ManagedObjects(tc.resourceInfo)
+			if tc.isErr {
+				require.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, objects)
+		})
+	}
+}
+
+type fakeResourceInfo struct {
+	err      error
+	infos    []*resource.Info
+	infosErr error
+}
+
+func (fri *fakeResourceInfo) Err() error {
+	return fri.err
+}
+
+func (fri *fakeResourceInfo) Infos() ([]*resource.Info, error) {
+	return fri.infos, fri.infosErr
 }
